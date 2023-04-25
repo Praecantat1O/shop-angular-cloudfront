18a9eca05671ca676f66e5dcd4f76fbde6f8c556
Stanislau Ukolau <Stanislau_Ukolau@epam.com>
2023-04-25 19:28:31 +0300

    [Task 5] Updated URLs for import-service

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -9,7 +9,7 @@ export const environment: Config = {
   apiEndpoints: {
     product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
+    import: 'https://on6if0p2mb.execute-api.us-east-1.amazonaws.com/development/',
     bff: 'https://q6mbpohyfl.execute-api.us-east-1.amazonaws.com/development/',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
