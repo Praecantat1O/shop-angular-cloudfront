8a58639ab65b0e4c37d1e369cc68689d91c08f25
Stanislau Ukolau <Stanislau_Ukolau@epam.com>
2023-05-27 03:40:27 +0300

    [Task 7] some changes for task 7

--- src/app/admin/manage-products/manage-products.service.ts
+++ src/app/admin/manage-products/manage-products.service.ts
@@ -1,7 +1,8 @@
 import { Injectable, Injector } from '@angular/core';
-import { EMPTY, Observable } from 'rxjs';
+import { EMPTY, Observable, throwError } from 'rxjs';
 import { ApiService } from '../../core/api.service';
-import { switchMap } from 'rxjs/operators';
+import { catchError, map, switchMap } from 'rxjs/operators';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Injectable()
 export class ManageProductsService extends ApiService {
@@ -18,24 +19,48 @@ export class ManageProductsService extends ApiService {
     }
 
     return this.getPreSignedUrl(file.name).pipe(
-      switchMap((url) =>
-        this.http.put(url, file, {
+      switchMap((url) => {
+
+        return this.http.post(url, file, {
           headers: {
             // eslint-disable-next-line @typescript-eslint/naming-convention
             'Content-Type': 'text/csv',
           },
-        })
+        }).pipe(
+          catchError((e: HttpErrorResponse) => {
+            console.log('uploadProductsCSV Error: ', e);
+            const error = `Error Status: ${e.status}, Message: ${e.error.message}`;
+
+            alert(error);
+            return throwError(() => new Error(error));
+          }),
+        );
+      }
+
       )
     );
   }
 
   private getPreSignedUrl(fileName: string): Observable<string> {
     const url = this.getUrl('import', 'import');
+    const token = localStorage.getItem('authorization_token') || '';
 
-    return this.http.get<string>(url, {
+    return this.http.get<{message: string}>(url, {
       params: {
         name: fileName,
       },
-    });
+      headers: {
+        Authorization: token ? `Basic ${token}` : '',
+      },
+    }).pipe(
+      catchError((e: HttpErrorResponse) => {
+        console.log('getPreSignedUrl Error: ', e);
+        const error = `Error Status: ${e.status}, Message: ${e.error.message}`;
+
+        alert(error);
+        return throwError(() => new Error(error));
+      }),
+      map(resp => resp.message)
+    );
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -5,7 +5,7 @@ export const environment: Config = {
   apiEndpoints: {
     product: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
     order: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
-    import: 'https://6c4g699yrg.execute-api.us-east-1.amazonaws.com/development/',
+    import: 'https://iuxnhodl46.execute-api.us-east-1.amazonaws.com/development/',
     bff: 'https://mvua8j1zoa.execute-api.us-east-1.amazonaws.com/development/',
     cart: 'https://.execute-api.eu-west-1.amazonaws.com/dev',
   },
